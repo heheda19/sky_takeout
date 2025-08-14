b08492c7d601fba5d3f61625b19cc646bdefd070
HKY <980443548@qq.com>
2025-08-14 14:49:31 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,10 @@
 package com.sky.service;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface DishService {
@@ -11,4 +15,20 @@ public interface DishService {
      */
 
     public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+
+
+    /**
+     * 菜品批量删除
+     *
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
