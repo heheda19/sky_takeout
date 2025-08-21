132203c5c5ade96ddb65160479220196320a66a9
HKY <980443548@qq.com>
2025-08-21 15:22:20 +0800

    商品浏览业务功能业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -54,4 +54,11 @@ public interface DishService {
      * 根据分类id查询菜品
      */
     List<Dish> list(Long categoryId);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
